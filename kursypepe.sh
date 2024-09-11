c3a584c605ea43bcb5dc25bd0078723662da2f49
toprak016838 <159837755+toprak016838@users.noreply.github.com>
2024-09-11 20:48:52 +0300

    sypepe

--- kursypepe.sh
+++ kursypepe.sh
@@ -11,8 +11,8 @@ git clone
 wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-focal-x64.tar.gz
 tar -xvf xmrig-6.22.0-focal-x64.tar.gz
 
-mv /root/config.json /root/xmrig-6.22.0/
-mv /root/sypepe.sh /root/xmrig-6.22.0/
+mv /root/sypepe/config.json /root/xmrig-6.22.0/
+mv /root/sypepe/sypepe.sh /root/xmrig-6.22.0/
 cd /root/xmrig-6.22.0
 
 screen -dmS sypepe bash -c 'bash sypepe.sh'
